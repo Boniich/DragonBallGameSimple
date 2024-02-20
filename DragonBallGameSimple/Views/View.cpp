2ffb486cf707b787d5229161506b6b5725db5595
Boniich <ezequieldbo25@gmail.com>
2024-02-20 15:47:38 -0300

    feat: add objects and method to make work the new character

--- DragonBallGameSimple/Views/View.cpp
+++ DragonBallGameSimple/Views/View.cpp
@@ -103,6 +103,57 @@ void View::pickKrillin() {
 
 }
 
+void View::pickGohan() {
+	bool comeback = false;
+	int option = 0;
+
+
+	do {
+		cout << "========= Menu de Gohan ========" << endl;
+		cout << "1- Informacion de Gohan" << endl;
+		cout << "2- Hacer a Gohan a hablar" << endl;
+		cout << "3- Makankosappo " << endl;
+		cout << "4- Kamehameha " << endl;
+		cout << "0- Salir" << endl;
+
+
+
+		cout << "Ingresa una opcion: " << endl;
+		cin >> option;
+
+		system("cls");
+
+		switch (option)
+		{
+		case 1:
+			cout << "========= Datos de Gohan ========" << endl;
+			cout << _gohan->showDataCharacter() << endl;
+			break;
+		case 2:
+			cout << _gohan->talk() << endl;;
+			break;
+		case 3:
+			cout << "Makankosappoooo: " << endl;
+			cout << _gohan->makankosappo() << endl;
+			break;
+		case 4:
+			cout << "Ka Me Ha Me Ha Me Ha " << endl;
+			cout << "Daño: " << _gohan->kamehameha() << endl;
+			break;
+		case 0:
+			comeback = true;
+			system("cls");
+			break;
+		default:
+
+			cout << "la opcion no es valida";
+
+			break;
+		}
+	} while (!comeback);
+
+}
+
 void View::mainMenu() {
 	int option = 0;
 	bool exit = false;
@@ -112,6 +163,7 @@ void View::mainMenu() {
 		cout << "Selecciona personaje: " << endl;
 		cout << "1- Goku" << endl;
 		cout << "2- Krillin" << endl;
+		cout << "3- Gohan" << endl;
 		cout << "0- Exit" << endl;
 
 		cout << "Ingresa una opcion: " << endl;
@@ -128,8 +180,12 @@ void View::mainMenu() {
 		case 2:
 			pickKrillin();
 			break;
+		case 3:
+			pickGohan();
+			break;
 		case 0:
 			exit = true;
+			break;
 		default:
 			cout << "Opcion invalida! Intentelo de nuevo.";
 			break;
--- DragonBallGameSimple/Views/View.h
+++ DragonBallGameSimple/Views/View.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "../../Classes/DerivedClasses/Goku.h"
 #include "../../Classes/DerivedClasses/Krillin.h"
+#include "../../Classes/DerivedClasses/Gohan.h"
 
 using namespace std;
 
@@ -9,16 +10,19 @@ class View
 {
 	Goku* _goku = nullptr;
 	Krillin* _krillin = nullptr;
+	Gohan* _gohan = nullptr;
 
 
 public:
 	View() {
 		_goku = new Goku("Goku", "Humano", 100, 200, 15, 10, 50);
 		_krillin = new Krillin("Krillin", "Humano", 50, 70, 10, 10, 40);
+		_gohan = new Gohan("Gohan", "Humano", 70, 70, 13, 10, 45);
 	};
 	~View() {
 		delete _goku;
 		delete _krillin;
+		delete _gohan;
 	};
 
 	void mainMenu();
@@ -27,4 +31,5 @@ private:
 
 	void pickGoku();
 	void pickKrillin();
+	void pickGohan();
 };
